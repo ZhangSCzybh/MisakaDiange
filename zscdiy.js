19e4daedb253ee4eac2e62cffc53d0499f8e7c9b
ZhangSCzybh <17858803001@163.com>
2025-03-13 14:59:25 +0800

    create zscjs

--- zscdiy.js
+++ zscdiy.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/zxs-ai/ZhangSCzybh/MisakaDiange/heads/main/zzmusicplay.js
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/ZhangSCzybh/MisakaDiange/heads/main/zzmusicplay.js
 
 [mitm]
 hostname = api.dragonlongzhu.cn
