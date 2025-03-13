9a50e3535222f29419c58701ab373c9d94b8fd0e
ZhangSCzybh <17858803001@163.com>
2025-03-13 15:25:28 +0800

    update

--- zscdiy1.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/ZhangSCzybh/MisakaDiange/refs/heads/main/zzmusicpaly.js
-
-[mitm]
-hostname = api.dragonlongzhu.cn
